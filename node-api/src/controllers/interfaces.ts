18dc18ca6e78206c982a470b9023d10d4dc58279
Juan Ruiz <jruizt96@gmail.com>
2026-07-10 17:44:11 -0500

    solo go

--- node-api/src/controllers/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import type { MatrixSet } from '../models/matrix';
-import type { Statistics } from '../models/statistics';
-
-/** Abstracción del caso de uso que usa el controlador, para no depender de su implementación. */
-export interface StatisticsService {
-    execute(matrices: MatrixSet): Statistics;
-}
--- node-api/src/models/matrix.ts
+++
@@ -1,5 +0,0 @@
-/** Una matriz es un arreglo de arreglos de números. */
-export type Matrix = number[][];
-
-/** Un conjunto de matrices sobre el que se calculan las estadísticas (p.ej. Q y R). */
-export type MatrixSet = Matrix[];
